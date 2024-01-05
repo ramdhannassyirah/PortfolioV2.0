c275b8de9488abe0274b587d3909f55d65378298
ramdhannassyirah <ramdhannassyirahh@gmail.com>
2024-01-05 23:05:55 +0700

    mobileNavbar

---
+++ components/mobileNavbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <section
+    id="mobileNavbar"
+    class="container flex flex-col justify-center min-h-screen mx-auto md:px-7"
+  >
+    <div class="">
+      <a href="" class="block text-6xl md:text-9xl font-DotGothic16">ABOUT</a>
+      <a href="" class="block text-6xl md:text-9xl font-DotGothic16"
+        >CONTACTS</a
+      >
+      <a
+        href=""
+        class="block text-6xl md:text-center md:text-9xl font-DotGothic16"
+        >PROJECTS</a
+      >
+      <a href="" class="block text-6xl md:text-9xl font-DotGothic16 md:text-end"
+        >HOME</a
+      >
+    </div>
+
+    <div
+      class="flex flex-col flex-wrap justify-between mt-10 font-mono md:flex-row"
+    >
+      <div class="logo">❤</div>
+      <div class="text-logo">
+        <h1 class="font-bold">Ramdhan Nassyirah</h1>
+      </div>
+      <div class="inline-flex gap-5 sosial-media">
+        <a href="">Instagram</a>
+        <a href="">Gmail</a>
+        <a href="">LinkedIn</a>
+      </div>
+    </div>
+  </section>
+</template>
+<style scoped></style>
