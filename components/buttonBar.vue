9625424b0581af0f8a8a662590bfc86ece7db26f
ramdhannassyirah <ramdhannassyirahh@gmail.com>
2024-01-07 18:29:48 +0700

    fixed

--- components/buttonBar.vue
+++ components/buttonBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <a href="#">
+  <a href="#" class="block md:hidden lg:hidden">
     <div
       class="w-[56px] h-[56px] bg-white rounded-full fixed z-40 bottom-10 right-5 flex items-center justify-center shadow-[5px_5px_0px_0px_rgba(56,49,47)]"
     >
