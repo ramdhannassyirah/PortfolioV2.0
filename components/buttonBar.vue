b62e99e01561569a727055f3bfea3d0deef51d77
ramdhannassyirah <ramdhannassyirahh@gmail.com>
2024-03-17 10:10:51 +0700

    chatroom

--- components/buttonBar.vue
+++ components/buttonBar.vue
@@ -1,11 +1,13 @@
 <template>
-  <a href="#" class="block md:hidden lg:hidden">
-    <div
-      class="button-54 w-[56px] h-[56px] bg-white rounded-full fixed z-40 bottom-10 right-5 flex items-center justify-center shadow-[5px_5px_0px_0px_rgba(56,49,47)]"
-    >
-      <img src="../assets/img/angle.svg" alt="" />
-    </div>
-  </a>
+  <div class="">
+    <NuxtLink to="/chatRoom" class="block md:hidden lg:hidden">
+      <div
+        class="button-54 w-[56px] h-[56px] bg-white rounded-full fixed z-40 bottom-10 right-5 flex items-center justify-center shadow-[5px_5px_0px_0px_rgba(56,49,47)]"
+      >
+        <img src="../assets/img/message.png" width="35px" alt="" />
+      </div>
+    </NuxtLink>
+  </div>
 </template>
 <style scoped>
 .button-54 {
--- pages/chatRoom.vue
+++ pages/chatRoom.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center min-h-screen">
+  <div class="flex flex-col justify-center">
     <div class="flex justify-center w-full">
       <main
         class="no-scrollbar w-full scroll-smooth transition-all px-5 lg:px-0 md:px-0 duration-300 lg:min-h-screen lg:max-w-[854px]"
@@ -9,6 +9,7 @@
           <p>Kirimkan Pesan Sekarang Juga</p>
         </div>
         <div
+          ref="chatContainer"
           class="no-scrollbar mb-4 h-[60vh] space-y-6 overflow-y-auto scroll-smooth border-b border-neutral-200 pb-2 dark:border-neutral-700 md:h-[65vh]"
         >
           <div class="flex flex-col gap-6">
@@ -26,6 +27,7 @@
           class="flex items-center justify-between w-full bg-[#fbeee0] rounded-full input"
         >
           <textarea
+            ref="messageInput"
             type="text"
             placeholder="Type a message"
             class="w-full p-2 text-xl placeholder:text-[#422800] font-bold bg-transparent outline-none"
@@ -40,11 +42,16 @@
       </main>
     </div>
   </div>
+  <div class="flex justify-center">
+    <NuxtLink class="my-10 text-2xl font-bold rounded-lg button-54" to="/"
+      >- Back -</NuxtLink
+    >
+  </div>
 </template>
 
 <script>
+import {collection, onSnapshot, addDoc} from "firebase/firestore";
 import db from "../firebase";
-import {collection, onSnapshot, getDocs, doc, addDoc} from "firebase/firestore";
 
 export default {
   data() {
@@ -53,14 +60,8 @@ export default {
       chatData: [],
     };
   },
-  head() {
-    return {
-      title: "Ramdhan || ChatRoom",
-    };
-  },
   async mounted() {
     try {
-      // Menggunakan listener untuk mendengarkan perubahan data secara real-time
       const unsubscribe = onSnapshot(collection(db, "users"), (snapshot) => {
         snapshot.docChanges().forEach((change) => {
           if (change.type === "added") {
@@ -70,13 +71,8 @@ export default {
               pesan: change.doc.data().pesan,
             };
             this.chatData.push(data);
-          }
-          if (change.type === "modified") {
-            const data = {
-              name: change.doc.id,
-              tanggal: change.doc.data().tanggal,
-              pesan: change.doc.data().pesan,
-            };
+
+            this.$nextTick(this.scrollChatContainer);
           }
         });
       });
@@ -84,10 +80,12 @@ export default {
       console.error("Error fetching data:", error);
     }
   },
-
   methods: {
     async sendData() {
-      const input = document.querySelector("textarea").value;
+      const input = this.$refs.messageInput.value.trim();
+      if (!input) {
+        return alert("Please enter a message");
+      }
       const currentDate = new Date();
       const tanggal = currentDate.toLocaleDateString("en-US", {
         year: "numeric",
@@ -95,15 +93,15 @@ export default {
         day: "numeric",
       });
 
-      if (!input) {
-        return alert("Please enter a message");
-      } else {
-        await addDoc(collection(db, "users"), {
-          pesan: input,
-          tanggal: tanggal,
-        });
-        document.querySelector("textarea").value = "";
-      }
+      await addDoc(collection(db, "users"), {
+        pesan: input,
+        tanggal: tanggal,
+      });
+      this.$refs.messageInput.value = ""; // Kosongkan textarea setelah pesan terkirim
+    },
+    scrollChatContainer() {
+      const container = this.$refs.chatContainer;
+      container.scrollTop = container.scrollHeight;
     },
   },
 };
@@ -113,6 +111,36 @@ export default {
 .input {
   box-shadow: #422800 4px 4px 0 0;
   border: 2px solid #422800;
+}
+
+.button-54 {
+  font-family: "Open Sans", sans-serif;
+  font-size: 16px;
+  letter-spacing: 2px;
+  text-decoration: none;
+  text-transform: uppercase;
+  color: #000;
+  cursor: pointer;
+  border: 3px solid;
+  padding: 0.25em 0.5em;
+  box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px,
+    5px 5px 0px 0px;
+  position: relative;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+.button-54:active {
+  box-shadow: 0px 0px 0px 0px;
+  top: 5px;
+  left: 5px;
+}
+
+@media (min-width: 768px) {
+  .button-54 {
+    padding: 0.25em 0.75em;
+  }
 }
 
 .button-74 {
